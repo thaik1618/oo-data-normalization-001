7165ba732649a5e8085e81ac82a3da95bc8cd0fb
Katie Hoffman <ktahoffman@gmail.com>
2014-08-22 14:57:16 -0400

    remove comments

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,3 @@
-# Implement a song class below.
-# The serialize method should replace spaces in 
-# the song title with underscores and write to 
-# the current working directory
 class Song
   attr_accessor :title, :artist
 
